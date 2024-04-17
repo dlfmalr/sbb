ddffb1e70b8cfeaa1364aa3272adf4ed5a18e304
dlfmalr <sonhyeseung05@gmail.com>
2024-04-17 17:40:18 +0900

    3-14(2)

---
+++ src/main/java/com/mysite/sbb/comment/Comment.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb.comment;
+
+import com.mysite.sbb.answer.Answer;
+import groovyjarjarpicocli.CommandLine;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Answer answer;
+
+
+}
