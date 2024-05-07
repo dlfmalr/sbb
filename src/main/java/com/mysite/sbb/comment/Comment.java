7a885a025ad5a6e22249354f83bd4b7d78c069c4
dlfmalr <sonhyeseung05@gmail.com>
2024-05-07 17:38:54 +0900

    3-14(2)ing

--- src/main/java/com/mysite/sbb/comment/Comment.java
+++ src/main/java/com/mysite/sbb/comment/Comment.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -32,5 +33,8 @@ public class Comment {
     @ManyToOne
     private SiteUser author;
 
+    private LocalDateTime modifyDate;
 
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- src/main/java/com/mysite/sbb/comment/CommentController.java
+++ src/main/java/com/mysite/sbb/comment/CommentController.java
@@ -1,18 +1,45 @@
 package com.mysite.sbb.comment;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerForm;
+import com.mysite.sbb.answer.AnswerService;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/comment")
 public class CommentController {
-    @GetMapping("/detail")
-    @ResponseBody
-    public String detail() {
-        return "detail test";
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+    private final UserService userService;
+    private final CommentService commentService;
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create/{id}")
+    public String createComment(Model model, @PathVariable("id") Integer id,
+                                @Valid CommentForm commentForm, BindingResult bindingResult,
+                                Principal principal, RedirectAttributes re) {
+        Answer answer = this.answerService.getAnswer(id);
+        Question question = this.questionService.getQuestion(answer.getQuestion().getId());
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("answer", answer);
+            return "answer_detail";
+        }
+        Comment comment = this.commentService.create(question, answer, commentForm.getContent(), siteUser);
+        return String.format("redirect:/answer/detail/%s#comment_%s", comment.getAnswer().getId(), comment.getId());
     }
 }
